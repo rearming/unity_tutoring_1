1627f016bac96addc55682545efe5e389bee2403
sleonard <sleonard@student.21-school.ru>
2020-09-26 12:57:02 +0300

    first lesson

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+	public Text textField;
+	public int score;
+    
+    void Start()
+    {
+        Debug.Log("Hello, I'm score!");
+        PrintScore();
+
+        textField.text = "hello !";
+
+        textField.fontSize = 70;
+    }
+
+    public void PrintScore()
+    {
+	    Debug.Log(score.ToString());
+    }
+
+    public void AddScore(int scoreToBeAdded)
+    {
+	    score += scoreToBeAdded;
+    }
+}
